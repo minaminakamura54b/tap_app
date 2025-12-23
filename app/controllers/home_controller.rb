4a5d38df5044cd156bb89e6728ccd552b625ac9a
Minami Nakamura <nakamura0373@gmail.com>
2025-12-23 00:19:59 -0800

    first

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+
+  def home
+  end
+  
+  def new
+  end
+
+  def about
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "home#home"
+  get 'home/new'
+  get 'home/about'
+  get 'home/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
